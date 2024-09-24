9fb1d951ffb16b25aabcf31b52d843f6a1c5b40f
suresh pilli <sureshpilli465@gmail.com>
2024-09-24 09:58:24 +0530

    fix: Fotter Mobile View

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,10 @@ function App() {
             <Route path='/' element={
               <div>
                 <Hero />
-                <Appointment />
-                <Stat />
                 <Research />
+                {/* <Blogs /> */}
+                <Stat />
+                <Appointment />
               </div>
             }
             />
